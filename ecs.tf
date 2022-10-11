5ca494c434f8699441f76e7c8fd86e7590a98be2
Camden <naviscam@gmail.com>
2022-10-10 22:08:54 -0400

    setup postgres with ecs

--- ecs.tf
+++ ecs.tf
@@ -17,9 +17,14 @@ resource "aws_ecs_task_definition" "prod_backend_web" {
       region     = var.region
       name       = "prod-backend-web"
       image      = aws_ecr_repository.backend.repository_url
-      command    = ["gunicorn", "-w", "3", "-b", ":8000", "django_aws.wsgi:application"]
+      command    = ["gunicorn", "-w", "4", "-b", ":8000", "django_aws.wsgi:application"]
       log_group  = aws_cloudwatch_log_group.prod_backend.name
       log_stream = aws_cloudwatch_log_stream.prod_backend_web.name
+
+      rds_db_name  = var.prod_rds_db_name
+      rds_username = var.prod_rds_username
+      rds_password = var.prod_rds_password
+      rds_hostname = aws_db_instance.prod.address
     },
   )
   execution_role_arn = aws_iam_role.ecs_task_execution.arn
@@ -35,6 +40,7 @@ resource "aws_ecs_service" "prod_backend_web" {
   deployment_maximum_percent         = 200
   launch_type                        = "FARGATE"
   scheduling_strategy                = "REPLICA"
+  enable_execute_command             = true
 
   load_balancer {
     target_group_arn = aws_lb_target_group.prod_backend.arn
@@ -86,6 +92,25 @@ resource "aws_iam_role" "prod_backend_task" {
       }
     ]
   })
+
+  inline_policy {
+    name = "prod-backend-task-ssmmessages"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = [
+            "ssmmessages:CreateControlChannel",
+            "ssmmessages:CreateDataChannel",
+            "ssmmessages:OpenControlChannel",
+            "ssmmessages:OpenDataChannel",
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
 }
 
 resource "aws_iam_role" "ecs_task_execution" {
